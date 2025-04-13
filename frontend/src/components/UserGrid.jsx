7827b76dfa593682f0a52f8c1def8c43553698e8
Enrique Shanna <enriqueshanna00@gmail.com>
2025-04-14 01:50:20 +0800

    3-connected frontend to backend

--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -1,8 +1,28 @@
-import { Grid } from '@chakra-ui/react';
+import { Flex, Grid, Text } from '@chakra-ui/react';
 import UserCard from './UserCard';
-import { USERS } from '../dummy/dummy';
+import { useEffect, useState } from 'react';
+import { BASE_URL } from '../App';
+const UserGrid = ({ users, setUsers }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch(BASE_URL + '/friends');
+        const data = await res.json();
 
-const UserGrid = () => {
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Grid
@@ -13,10 +33,20 @@ const UserGrid = () => {
         }}
         gap={4}
       >
-        {USERS.map((user) => (
-          <UserCard key={user.id} user={user} />
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} setUsers={setUsers} />
         ))}
       </Grid>
+      {!isLoading && users.length === 0 && (
+        <Flex justifyContent={'center'}>
+          <Text fontSize={'xl'}>
+            <Text as={'span'} fontSize={'2xl'} fontWeight={'bold'} mr={2}>
+              Poor you! 🥺
+            </Text>
+            No friends found.
+          </Text>
+        </Flex>
+      )}
     </>
   );
 };
